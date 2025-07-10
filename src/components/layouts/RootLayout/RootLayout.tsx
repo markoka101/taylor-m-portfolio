1c93cf44502c33342c5bfd8c7dade98a4d30721f
markoka101 <markoka101@gmail.com>
2025-07-10 17:00:46 -0400

    adjusting motion to allow entrance and exit animations without double

--- src/components/layouts/RootLayout/RootLayout.tsx
+++ src/components/layouts/RootLayout/RootLayout.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../../Header';
@@ -22,9 +23,20 @@ export default function RootLayout() {
 			)}
 			<div className="relative z-10 flex w-screen">
 				{/* Removed AnimatePresence to prevent double fade in */}
-				<div className="flex w-full justify-center">
+				<AnimatePresence mode="wait">
+					<motion.main
+						key={location.pathname.split('/')[1] || 'root'}
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 0.25 }}
+					>
+						<Outlet />
+					</motion.main>
+				</AnimatePresence>
+				{/* <div className="flex w-full justify-center">
 					<Outlet />
-				</div>
+				</div> */}
 			</div>
 		</main>
 	);
