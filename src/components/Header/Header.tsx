7534dc189ca19488149db559eae55a3ca71ea6aa
markoka101 <markoka101@gmail.com>
2025-07-11 17:26:56 -0400

    opted for css animations

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import { motion, type Variants } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logos/taylor-header-logo.avif';
 
@@ -7,28 +6,15 @@ export interface HeaderProps {
 	isMenuOpen?: boolean;
 }
 
-//50/50 on removing depending on added complexity
-const logoVariants: Variants = {
-	hidden: { opacity: 0 },
-	visible: { opacity: 1 }
-};
-
 export default function Header() {
 	return (
-		<header className="top-0 flex h-28 w-screen flex-col justify-start pt-6 lg:px-12">
-			{/* fade in only for logo and name in header */}
-			<motion.div
-				className="flex items-center text-2xl font-bold text-green-600"
-				variants={logoVariants}
-				initial="hidden"
-				animate="visible"
-				transition={{ duration: 0.6 }}
-			>
+		<header className="animate-fade-in top-0 flex h-28 w-screen flex-col justify-start pt-6 lg:px-12">
+			<div className="flex items-center text-2xl font-bold text-green-600">
 				<img src={logo} alt="Taylor Morales logo" className="h-full w-auto" />
 				<Link to="/" className="font-marmelad pl-4 text-3xl font-light text-white">
 					Taylor Morales
 				</Link>
-			</motion.div>
+			</div>
 			<hr className="text-text/40 mt-5 w-full rounded" />
 		</header>
 	);
