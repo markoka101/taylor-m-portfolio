d01462b96477a55f2a6efea5d4ecacb560b9cd96
markoka101 <markoka101@gmail.com>
2025-07-14 11:34:45 -0400

    changed from slide to fade in navbar

--- src/components/Navigation/MobileNav.tsx
+++ src/components/Navigation/MobileNav.tsx
@@ -3,8 +3,8 @@ import type { NavbarProps } from './Navbar';
 function MobileNav({ isOpen, setIsOpen }: NavbarProps) {
 	return (
 		<nav
-			className={`bg-accent fixed top-0 right-0 z-40 h-screen w-[75%] transform overflow-y-auto pt-2 pl-6 text-white transition-transform duration-500 ease-in-out sm:w-[350px] md:w-[400px] ${
-				!isOpen ? 'translate-x-full' : 'translate-x-0'
+			className={`bg-accent fixed top-0 right-0 z-40 h-screen w-screen transform overflow-y-auto pt-2 pl-6 text-white transition-all duration-1000 ease-in-out lg:hidden ${
+				!isOpen ? 'pointer-events-none opacity-0' : 'pointer-events-auto opacity-100'
 			}`}
 			aria-label="Mobile Navigation"
 		></nav>
